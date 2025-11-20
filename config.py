dd1909f6a773290be5c2b92a76e5712b08ad578c
Zheng Luo <luozheng2002@sjtu.edu.cn>
2025-11-20 00:37:21 -0600

    collected failed data

--- config.py
+++ config.py
@@ -26,4 +26,6 @@ class Config:
 
 configs = [
     Config(Model.QWEN_2_5_7B_INSTRUCT, "zh_cn", "en", "philosophy", ResultType.PREFERENCE_DIRECT),
+    Config(Model.QWEN_2_5_7B_INSTRUCT, "zh_cn", "en", "philosophy", ResultType.PREFERENCE_THINKING),
+    Config(Model.QWEN_2_5_7B_INSTRUCT, "zh_cn", "en", "philosophy", ResultType.PERPLEXITY),
 ]
